c29c3be6bad90c1bd7198cc74b432aea9a93d63e
FernandoAndrada <fernandojcandrada@gmail.com>
2022-09-28 14:31:08 -0300

    2

---
+++ main/main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def teste_3 (name):
+    # Use a breakpoint in the code line below to debug your script.
+   st.write("""  # Teste Streamlit """)
+
+   st.write(""" Teste Botões """)
+
+   window = st.slider(" # Botão slider ")
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    teste_3 ('PyCharm')
+
